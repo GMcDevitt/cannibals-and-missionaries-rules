46b75c3df0e9de406edf054e4f72832862212acd
Gerard McDevitt <gmcdevitt@pinnacle21.net>
2016-10-26 11:30:59 -0400

    Attempt to solve the problem in three rules

--- missionary-cannibals-rules.lisp
+++ missionary-cannibals-rules.lisp
@@ -1,15 +1,9 @@
 
 (defconstant *rules* 
 	'( 
-		((B C *Y M R)			->	(*Y R B M C)			R1) ;; **BEGIN**, Missionary 1 takes Cannibal 1 over
-		((*Y R B C M)			->	(B *Y M R C)			R2) ;; Missionary 1 comes back for Missionary 2
-		((B C *Y M R C)			->	(*Y R B C M M)			R3) ;; Missionary 1 takes Missionary 2 over
-		((*Y R B C M M)			->	(B C *Y R C M)			R4) ;; Missionary 1 comes back for Missionary 3
-		((B C *Y R C M)			->	(C C R B *Y M)			R5) ;; Missionary 1 takes Missionary 3 over
-		((C C R B *Y M)			->	(B C C M R *Y)			R6) ;; Missionary 1 comes back for Cannibal 2
-		((B C C M R *Y)			->	(C R B C *Y M)			R7) ;; Missionary 1 takes Cannibal 2 over
-		((C R B *Y M)			->	(B C M R *Y)			R8) ;; Missionary 1 comes back for Cannibal 3
-		((B C M R *Y)			->	(R B C *Y M)			R9) ;; Missionary 1 and Cannibal 3 both go over, **END** Horray! No one was eaten!
+		((B C *Y M R *X)		->	(*Y R B C *X M)			LR1) ;; a Missionary takes a Cannibal over
+		((*Y R B *X M)			->	(B *Y M R *X)			LR2) ;; Bring a missionary back
+		((B *Y M M R *X)		->	(*Y R B *X M M)			LR3) ;; Missionary brings a missionary over
 	)
 )
 
